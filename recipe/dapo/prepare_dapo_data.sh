36e831959aefbb583864fde0ab25468b30c35a42
molumolua <1005389104@qq.com>
2025-04-26 19:27:12 +0800

    Initial commit

---
+++ recipe/dapo/prepare_dapo_data.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -uxo pipefail
+
+export VERL_HOME=${VERL_HOME:-"${HOME}/verl"}
+export TRAIN_FILE=${TRAIN_FILE:-"${VERL_HOME}/data/dapo-math-17k.parquet"}
+export TEST_FILE=${TEST_FILE:-"${VERL_HOME}/data/MATH-500.parquet"}
+
+mkdir -p "${VERL_HOME}/data"
+
+#wget -O "${TRAIN_FILE}" "https://hf-mirror.com/datasets/BytedTsinghua-SIA/DAPO-Math-17k/resolve/main/data/dapo-math-17k.parquet?download=true"
+
+wget -O "${TEST_FILE}" "https://hf-mirror.com/datasets/HuggingFaceH4/MATH-500/blob/main/test.jsonl"
